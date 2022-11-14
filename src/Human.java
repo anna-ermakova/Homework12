739f72df3f2f4e4897ddf070e5400cfe7883ac4d
anna-ermakova <korovanna8@mail.ru>
2022-11-14 16:15:52 +0300

    Курс 2. Первая домашка по ООП. ДЗ1: задания 1-2 /Human/

---
+++ src/Human.java
@@ -0,0 +1,24 @@
+public class Human {
+
+
+    public int yearOfBirth;
+    public String name;
+    public String town;
+    public String jobTitle;
+
+
+
+
+    public Human(int yearOfBirth, String name, String town, String jobTitle) {
+        this.yearOfBirth = yearOfBirth;
+        this.name = name;
+        this.town = town;
+        this.jobTitle = jobTitle;
+    }
+
+    public String toString() {
+        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности "+jobTitle+ ". Будем знакомы!";
+    }
+
+
+}
