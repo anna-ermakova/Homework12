8dac4e96d26c8ee18ce8518198fb22d641ef04ba
anna-ermakova <korovanna8@mail.ru>
2022-11-15 17:01:58 +0300

    Курс 2. Первая домашка по ООП. Human

--- src/Human.java
+++ src/Human.java
@@ -1,23 +1,35 @@
+
 public class Human {
 
 
-    public int yearOfBirth;
-    public String name;
-    public String town;
-    public String jobTitle;
-
-
+    private int yearOfBirth;
+    private String name;
+    private String town;
+    private String jobTitle;
 
 
     public Human(int yearOfBirth, String name, String town, String jobTitle) {
-        this.yearOfBirth = yearOfBirth;
-        this.name = name;
-        this.town = town;
-        this.jobTitle = jobTitle;
+        if (yearOfBirth<0) {
+            yearOfBirth = 0;
+        }
+            this.yearOfBirth = yearOfBirth;
+       if (name=="" || name==null){
+           name = "Информация не указана";
+       }
+           this.name = name;
+        if (town=="" || town==null) {
+            town="Информация не указана";
+        }
+       this.town = town;
+        if (jobTitle=="" || jobTitle==null) {
+            jobTitle="Информация не указана";
+        }
+       this.jobTitle = jobTitle;
     }
 
+
     public String toString() {
-        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности "+jobTitle+ ". Будем знакомы!";
+        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в " + yearOfBirth + " году. Я работаю на должности " + jobTitle + ". Будем знакомы!";
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,19 @@
 public class Main {
+    public static Human[] human;
     public static void main(String[] args) {
 
-        Human human1 = new Human (1988, "Максим", "Минск", "бренд-менеджер");
-        Human human2 = new Human(1993, "Аня", "Москва", "методист образовательных программ");
-        Human human3 = new Human(1992, "Катя", "Калининград", "продакт-менеджер");
-        Human human4 = new Human(1995, "Артем", "Москва", "директор по развитию бизнеса");
-        System.out.println(human1);
-        System.out.println(human2);
-        System.out.println(human3);
-        System.out.println(human4);
-
-
+        human = new Human[4];
+        human[0] = new Human(1988, "Максим", "Минск", "бренд-менеджер");
+        human[1] = new Human(1993, "Аня", "Москва", "методист образовательных программ");
+        human[2] = new Human(1992, "Катя", "Калининград", "продакт-менеджер");
+        human[3] = new Human(1995, "Артем", "Москва", "директор по развитию бизнеса");
+        printHumans();
+    }
+    public static void printHumans() {
+        for (int i = 0; i < human.length; i++) {
+            if (human[i] != null) {
+                System.out.println(human[i].toString());
+            }
+        }
     }
 }
